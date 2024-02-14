5e0ab9c75268312b139d938b3107734fb553f6a0
Luis Castro <luisangelcastrocabrera@gmail.com>
2024-02-14 03:27:37 -0500

    Project Creation

---
+++ src/main/java/com/mycompany/edusena2024/EDUSENA2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.edusena2024;
+
+/**
+ *
+ * @author Familia Chichona
+ */
+public class EDUSENA2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
